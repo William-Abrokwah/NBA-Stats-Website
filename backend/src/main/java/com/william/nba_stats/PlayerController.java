6a9e756ed20ee05cf0326949c1d91af41f17b5e2
William Abrokwah <abrokwah27e@gmail.com>
2026-05-25 20:02:09 -0400

    Add a few GET and DE;ETE requests

--- backend/src/main/java/com/william/nba_stats/PlayerController.java
+++ backend/src/main/java/com/william/nba_stats/PlayerController.java
@@ -2,14 +2,30 @@ package com.william.nba_stats;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/player")
 public class PlayerController {
-    private final PlayerService playerService;
+    private final PlayerService service;
 
     @Autowired
-    public PlayerController(PlayerService playerService) {
-        this.playerService = playerService;
+    public PlayerController(PlayerService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Player> getPlayers() {
+        return service.getPlayers();
+    }
+
+    @GetMapping("/{id}")
+    public Player getPlayerById(@PathVariable Long id) {
+        return service.getPlayerById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePlayer(@PathVariable Long id) {
+        service.deletePlayerById(id);
     }
 }
--- backend/src/main/java/com/william/nba_stats/PlayerService.java
+++ backend/src/main/java/com/william/nba_stats/PlayerService.java
@@ -1,8 +1,28 @@
 package com.william.nba_stats;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class PlayerService {
+    private final PlayerRepository repository;
 
+    @Autowired
+    public PlayerService(PlayerRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Player> getPlayers() {
+        return repository.findAll();
+    }
+
+    public Player getPlayerById(Long id) {
+        return repository.findById(id).orElseThrow(() ->
+        new IllegalStateException("Student not found"));
+    }
+
+    public void deletePlayerById(Long id) {
+        repository.deleteById(id);
+    }
 }
